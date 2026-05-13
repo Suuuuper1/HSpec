0e9e2a0a2dc42fa0d7bfd2adec51626ea0b4abba
Super__1 <Super__1@noreply.gitcode.com>
2026-05-13 09:10:33 +0800

    update prefetch batch

--- llm_rl/qwen3/scripts/train_grpo_qwen3_30b_16die_true_weight.sh
+++ llm_rl/qwen3/scripts/train_grpo_qwen3_30b_16die_true_weight.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# export ASCEND_LAUNCH_BLOCKING=1
+export ASCEND_LAUNCH_BLOCKING=1
 
 # Model and dataset
 HOME=$(pwd)
